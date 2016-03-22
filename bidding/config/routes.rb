dbf617a31ac1fee729e050bf6947b56adb8cb9de
niko <njichev@gmail.com>
2016-03-22 15:26:23 +0200

    Add bootstrap and remembered that I am working with Products instead of Items, changed routes accordingly.

--- bidding/config/routes.rb
+++ bidding/config/routes.rb
@@ -7,6 +7,6 @@ Rails.application.routes.draw do
   post 'login', to: 'sessions#create'
   delete 'logout', to: 'sessions#destroy'
 
-  resources :items
-  root 'items#index'
+  resources :products
+  root 'products#index'
 end
